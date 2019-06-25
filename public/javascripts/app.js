4dcf0a8da4c7a436435e192e4156190c95ba0262
Eric Tovar <tovareric@gmail.com>
2019-06-24 23:28:58 -0500

    setting up app

---
+++ public/javascripts/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var messages = document.getElementById('messages');
+    var latestMsg = document.getElementById('latest-msg');
+    var userName = document.getElementById('user');
+
+    var socket = io();
+    socket.on('add-message', function (data) {
+        addMessage(data);
+    });
+
+    document.getElementById('send-msg-btn').addEventListener('click', function(){
+        socket.emit('add-message', {
+            name: userName.value,
+            msg: latestMsg.value
+        });
+        latestMsg.value = '';
+    });
+
+    function addMessage(data){
+        messages.innerHTML += ['<li>', data.name, ':', data.msg + '</li>'].join('');
+    }
+    
+});
+
